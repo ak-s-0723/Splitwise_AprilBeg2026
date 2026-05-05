7ddfa6b9c9e2c3177811c338b3fd7718a8f2df2b
khannaanurag1 <khannaanurag1@gmail.com>
2026-05-05 22:59:47 +0530

    initial commit

---
+++ src/main/java/org/example/splitwise_april2026beg/SplitwiseApril2026BegApplication.java
@@ -0,0 +1,13 @@
+package org.example.splitwise_april2026beg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseApril2026BegApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitwiseApril2026BegApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/splitwise_april2026beg/SplitwiseApril2026BegApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.splitwise_april2026beg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitwiseApril2026BegApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
